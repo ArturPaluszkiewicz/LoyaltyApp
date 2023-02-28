a1591da734b6b165332688c7fffec67324fd6e26
Arturro <arturro_1996@o2.pl>
2023-02-28 04:05:56 +0100

    Add endpoint to search Receipt by BindToClient set on True

--- src/main/java/com/arturrodev4/loyaltyapp/model/ReceiptRepository.java
+++ src/main/java/com/arturrodev4/loyaltyapp/model/ReceiptRepository.java
@@ -2,7 +2,12 @@ package com.arturrodev4.loyaltyapp.model;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+
+import java.util.List;
 
 @RepositoryRestResource
 public interface ReceiptRepository extends JpaRepository<Receipt, Integer> {
+    @RestResource(path = "Bind")
+    List<Receipt> findByBindToClientIsTrue();
 }

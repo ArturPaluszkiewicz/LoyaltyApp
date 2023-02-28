4bd789be4ec2b5d4076fcd79efcde03f1a728f5f
Arturro <arturro_1996@o2.pl>
2023-02-28 04:45:24 +0100

    Make new endpoint for reading receipts.

--- src/main/java/com/arturrodev4/loyaltyapp/controller/ReceiptController.java
+++ src/main/java/com/arturrodev4/loyaltyapp/controller/ReceiptController.java
@@ -2,6 +2,8 @@ package com.arturrodev4.loyaltyapp.controller;
 
 import com.arturrodev4.loyaltyapp.model.ReceiptRepository;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @RepositoryRestController
 public class ReceiptController {
@@ -9,5 +11,10 @@ public class ReceiptController {
 
     public ReceiptController(final ReceiptRepository receiptRepository) {
         this.receiptRepository = receiptRepository;
+    }
+
+    @GetMapping("/receipts")
+    ResponseEntity<?> readAllReceipt(){
+        return ResponseEntity.ok(receiptRepository.findAll());
     }
 }

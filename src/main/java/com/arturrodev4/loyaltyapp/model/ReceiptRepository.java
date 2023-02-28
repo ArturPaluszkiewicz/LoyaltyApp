51f53b483891c8945f96f634f6b247167880b2b6
Arturro <arturro_1996@o2.pl>
2023-02-28 04:14:56 +0100

    Change endpoint to search Receipt on to search by BindToClient with param(state) true or false

--- src/main/java/com/arturrodev4/loyaltyapp/model/ReceiptRepository.java
+++ src/main/java/com/arturrodev4/loyaltyapp/model/ReceiptRepository.java
@@ -1,6 +1,7 @@
 package com.arturrodev4.loyaltyapp.model;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
 
@@ -9,5 +10,5 @@ import java.util.List;
 @RepositoryRestResource
 public interface ReceiptRepository extends JpaRepository<Receipt, Integer> {
     @RestResource(path = "Bind")
-    List<Receipt> findByBindToClientIsTrue();
+    List<Receipt> findByBindToClient(@Param("state") boolean bind);
 }
